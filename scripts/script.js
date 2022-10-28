15438f9536ff64c106f756070aaa1eafb68a7c55
monkey <sam.corominas@gmail.com>
2022-10-28 19:20:54 +1100

    initial commit all pages complete for desktop

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const path = document.querySelector('path')
+const pathLength = path.getTotalLength()
+
+path.style.strokeDasharray = pathLength + ' ' + pathLength
+
+path.style.strokeDashoffset = pathLength
+
+window.addEventListener('scroll', () => {
+
+  let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)
+
+  let drawLength = pathLength * (scrollPercentage + 0.02)
+
+  console.log(scrollPercentage)
+
+  path.style.strokeDashoffset = pathLength + drawLength
+})
