8f155fd7a9646e5fe5e4885ef3eec8fd91ea981c
monkey <sam.corominas@gmail.com>
2022-11-10 18:51:29 +1100

    font page styling improved with css grid - grid is so good I can't belive I only just found out. Apple puzzle added. door on about page commented out

---
+++ scripts/door.js
@@ -0,0 +1,20 @@
+$('.apple-one').click(
+  () => {
+    $('.apple-counter-one').removeClass('grey')
+  }
+)
+$('.apple-two').click(
+  () => {
+    $('.apple-counter-two').removeClass('grey')
+  }
+)
+$('.apple-three').click(
+  () => {
+    $('.apple-counter-three').removeClass('grey')
+  }
+)
+$('.apple-four').click(
+  () => {
+    $('.apple-counter-four').removeClass('grey')
+  }
+)
